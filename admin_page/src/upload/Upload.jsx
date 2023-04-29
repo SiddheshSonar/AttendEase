4313f82bf5be84c905717facbdff0ee94da74bf0
Vi-shub <Smsharma3121@gmail.com>
2023-04-29 15:23:22 +0530

    upload form completed

--- admin_page/src/upload/Upload.jsx
+++ admin_page/src/upload/Upload.jsx
@@ -1,38 +1,73 @@
-import React, { useState } from 'react';
-import NavB from '../Navbar';
+import React, { useState } from "react";
+import NavB from "../Navbar";
 
 const Upload = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
 
-    function handleFile(event) {
-        setFile(event.target.files[0]);
-    }
+  function handleFile(event) {
+    setFile(event.target.files[0]);
+  }
 
-    function handleUpload() {
-        setLoading(true);
-        const formData = new FormData();
-        formData.append('file', file);
-        fetch('http://localhost:5000/upload', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error))
-            .finally(() => {
-                setLoading(false);
-            });
-    }
+  function handleDrop(event) {
+    event.preventDefault();
+    const droppedFile = event.dataTransfer.files[0];
+    setFile(droppedFile);
+  }
 
-    return (
-        <div>
-            <NavB/>
-            <div className='upload-container'>
-            <input className="" type="file" onChange={handleFile} />
-            <button className="btn btn-primary" onClick={handleUpload}>UPLOAD FILE</button>
-            </div>
+  function handleUpload() {
+    // setLoading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUploadStatus(data.message);
+        console.log(data);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        // setLoading(false);
+      });
+  }
+
+  return (
+    <div>
+      <NavB />
+      <h1 className="title">Upload Your File</h1>
+      <div className="upload-container">
+        <div
+          className="dropzone"
+          onDragOver={(event) => event.preventDefault()}
+          onDrop={handleDrop}
+        >
+          {file ? (
+            <p className="title-font">{file.name}</p>
+          ) : (
+            <p>Drag and drop a file here or click to select a file.</p>
+          )}
         </div>
-    );
+        <input type="file" onChange={handleFile} accept=".csv" style={{ display: "none" }} />
+        <button
+          className="btn btn-primary"
+          onClick={() => document.querySelector('input[type="file"]').click()}
+        >
+          SELECT FILE
+        </button>
+        <button
+          className="btn btn-primary"
+          disabled={!file}
+          onClick={handleUpload}
+        >
+          UPLOAD FILE
+        </button>
+        {uploadStatus && <p>{uploadStatus}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
